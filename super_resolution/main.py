ae34cbdce5955a93d508dd6971f5266d72d1ded4
Francisco R Castro G <francisco.castro.garcia@intel.com>
2025-05-16 00:09:38 +0000

    Fix typo in 'accelerated' word in help parameter for main function

--- super_resolution/main.py
+++ super_resolution/main.py
@@ -16,7 +16,7 @@ parser.add_argument('--batchSize', type=int, default=64, help='training batch si
 parser.add_argument('--testBatchSize', type=int, default=10, help='testing batch size')
 parser.add_argument('--nEpochs', type=int, default=2, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
-parser.add_argument('--accel', action='store_true', default=False, help='enables Acceleterated training')
+parser.add_argument('--accel', action='store_true', default=False, help='enables Accelerated training')
 parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
 parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
 opt = parser.parse_args()
